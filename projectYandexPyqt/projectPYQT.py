01644ab2dd2563e634812f33f7b9a13e809c2a95
syzyshimer <shimerr142@gmail.com>
2024-11-22 15:28:28 +0200

    'refresh'

--- projectYandexPyqt/projectPYQT.py
+++ projectYandexPyqt/projectPYQT.py
@@ -6,7 +6,6 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QLabel, QTableWidgetItem
 import sqlite3
 
-
 class WorkoutTracker(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -26,51 +25,39 @@ class WorkoutTracker(QMainWindow):
 
         self.timeTable.setColumnCount(6)
         self.timeTable.setHorizontalHeaderLabels(['Номер', 'Дата', 'Калории','Продолжительность', 'Тип', 'Описание'])
-        self.timeTable.resizeRowsToContents()
-        self.timeTable.resizeColumnsToContents()
-        self.timeTable.horizontalHeader().setStretchLastSection(True)
 
-        self.setFixedSize(834, 580)
+        self.setFixedSize(986, 614)
 
-        self.window1 = Warmup()
-        self.window2 = Arms()
-        self.window3 = Legs()
-        self.window4 = Back()
-        self.window5 = Breast()
+        self.warmup.clicked.connect(lambda: self.show_body('warmup'))
+        self.arms.clicked.connect(lambda: self.show_body('arms'))
+        self.legs.clicked.connect(lambda: self.show_body('legs'))
+        self.back.clicked.connect(lambda: self.show_body('back'))
+        self.breast.clicked.connect(lambda: self.show_body('breast'))
 
-        self.warmup.clicked.connect(self.show_warmup)
-        self.arms.clicked.connect(self.show_arms)
-        self.legs.clicked.connect(self.show_legs)
-        self.back.clicked.connect(self.show_back)
-        self.breast.clicked.connect(self.show_breast)
         self.load_data()
+
+    def show_body(self, part):
+        self.body_window = Body(part)
+        if self.body_window.isVisible():
+            self.body_window.hide()
+        else:
+            self.body_window.show()
 
     def load_data(self):
         data = self.db.fetch_data()
-        self.timeTable.setRowCount(len(data))
-        row = 0
-        for el in data:
-            id = data.value(0)
-            date = data.value(1)
-            calories = data.value(2)
-            long = data.value(3)
-            type = data.value(4)
-            description = data.value(5)
 
-            self.timeTable.insertRow(row)
-            self.timeTable.setItem(row, 0, QTableWidgetItem(str(id)))
-            self.timeTable.setItem(row, 1, QTableWidgetItem(date))
-            self.timeTable.setItem(row, 2, QTableWidgetItem(str(calories)))
-            self.timeTable.setItem(row, 3, QTableWidgetItem(str(long)))
-            self.timeTable.setItem(row, 4, QTableWidgetItem(str(type)))
-            self.timeTable.setItem(row, 5, QTableWidgetItem(str(description)))
-            row += 1
+        if data:
+            self.timeTable.setRowCount(len(data))
+            self.timeTable.setColumnCount(len(data[0]))
+            for row_index, row_data in enumerate(data):
+                for column_index, item in enumerate(row_data):
+                    self.timeTable.setItem(row_index, column_index, QTableWidgetItem(str(item)))
 
     def add_workout(self):
         date = self.currentDate.date().toString('yyyy-MM-dd')
-        calories = self.cal_info.text()
-        long = self.long_info.text()
+        calories = float(self.cal_info.text())
         type = self.type_info.text()
+        long = float(self.long_info.text())
         discription = self.disc_info.text()
 
         self.db.add_data(date, calories, long, type, discription)
@@ -81,107 +68,55 @@ class WorkoutTracker(QMainWindow):
         self.type_info.clear()
         self.disc_info.clear()
 
-        self.load_table()
-
-    def button_click(self):
-        self.add_btn.clicked.connect(self.add_workout)
-        self.del_btn.clicked.connect(self.delete_workout)
+        self.load_data()
 
     def delete_workout(self):
         selected_row = self.timeTable.currentRow()
 
         if selected_row == -1:
-            QMessageBox.warning(self, "ОШИБКА", "Пожалуйста, выберите ряд")
+            QMessageBox.warning(self, "Ошибка", "Пожалуйста, выберите ряд!")
 
-        id = int(self.timeTable.item(selected_row, 0).text())
-        confirm = QMessageBox.question(self, "Вы уверены?", 'Удалить данную тренировку', QMessageBox.Yes | QMessageBox.No)
+        work_id = int(self.timeTable.item(selected_row, 0).text())
+        confirm = QMessageBox.question(self, "Вы уверены?", 'Удалить данную тренировку?', QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
 
-        if confirm == QMessageBox.No:
-            return
+        if confirm == QMessageBox.StandardButton.Yes:
+            self.db.delete_data(work_id)
+            self.load_data()
 
-    def show_warmup(self):
-        if self.window1.isVisible():
-            self.window1.hide()
-        else:
-            self.window1.show()
 
-    def show_arms(self):
-        if self.window2.isVisible():
-            self.window2.hide()
-        else:
-            self.window2.show()
+    def update_workout(self):
+        selected_row = self.timeTable.currentRow()
 
-    def show_legs(self):
-        if self.window3.isVisible():
-            self.window3.hide()
-        else:
-            self.window3.show()
+        if selected_row == -1:
+            QMessageBox.warning(self, "Ошибка", "Пожалуйста, выберите ряд!.")
 
-    def show_back(self):
-        if self.window4.isVisible():
-            self.window4.hide()
-        else:
-            self.window4.show()
+        work_id = int(self.timeTable.item(selected_row, 0).text())
+        date = self.currentDate.date().toString('yyyy-MM-dd')
+        calories = float(self.cal_info.text())
+        type = self.type_info.text()
+        long = float(self.long_info.text())
+        discription = self.disc_info.text()
 
-    def show_breast(self):
-        if self.window5.isVisible():
-            self.window5.hide()
-        else:
-            self.window5.show()
+        self.db.update_data(work_id, date, calories, type, long, discription)
+        self.load_data()
 
-class Warmup(QMainWindow):
-    def __init__(self):
+    def button_click(self):
+        self.add_btn.clicked.connect(self.add_workout)
+        self.del_btn.clicked.connect(self.delete_workout)
+        self.update_btn.clicked.connect(self.update_workout)
+
+class Body(QMainWindow):
+    def __init__(self, part):
+        self.pictures = {'warmup': 'warmup.jpg', 'arms': 'arms.png', 'legs': 'legs.png',
+                         'back': 'back.jpg', 'breast': 'breast.png'}
         super().__init__()
         self.setWindowTitle('Руки')
         self.label = QLabel(self)
-        self.pixmap = QPixmap('warmup.jpg')
+        self.pixmap = QPixmap(self.pictures[part])
         self.label.setPixmap(self.pixmap)
         self.setCentralWidget(self.label)
         self.setFixedSize(self.pixmap.width(), self.pixmap.height())
 
-
-class Arms(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Руки')
-        self.label = QLabel(self)
-        self.pixmap = QPixmap('arms.png')
-        self.label.setPixmap(self.pixmap)
-        self.setCentralWidget(self.label)
-        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
-
-
-class Legs(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Руки')
-        self.label = QLabel(self)
-        self.pixmap = QPixmap('legs.png')
-        self.label.setPixmap(self.pixmap)
-        self.setCentralWidget(self.label)
-        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
-
-
-class Back(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Руки')
-        self.label = QLabel(self)
-        self.pixmap = QPixmap('back.jpg')
-        self.label.setPixmap(self.pixmap)
-        self.setCentralWidget(self.label)
-        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
-
-
-class Breast(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Руки')
-        self.label = QLabel(self)
-        self.pixmap = QPixmap('breast.png')
-        self.label.setPixmap(self.pixmap)
-        self.setCentralWidget(self.label)
-        self.setFixedSize(self.pixmap.width(), self.pixmap.height())
 
 class DataBase:
     def __init__(self):
@@ -197,34 +132,34 @@ class DataBase:
                 date TEXT NOT NULL,
                 calories REAL,
                 long REAL,
-                type REAL,
+                type TEXT,
                 description TEXT
                 )
                 ''')
         con.commit()
         con.close()
 
-    def add_data(date, calories, long, type, disc):
+    def add_data(self, date, calories, long, type, disc):
         con = sqlite3.connect('workouts.db')
         cur = con.cursor()
-        query = f"INSERT INTO workout (date, calories, long, type, discr) VALUES (?, ?, ?, ?, ?)"
+        query = f"INSERT INTO workout (date, calories, long, type, description) VALUES (?, ?, ?, ?, ?)"
         cur.execute(query, (date, calories, long, type, disc))
         con.commit()
         con.close()
 
-    def delete_data(self):
+    def delete_data(self, work_id):
         con = sqlite3.connect('workouts.db')
         cur = con.cursor()
         query = f'DELETE FROM workout WHERE id = ?'
-        cur.execute(query)
+        cur.execute(query, (work_id,))
         con.commit()
         con.close()
 
-    def update_record(date, calories):
+    def update_data(self, work_id, date, calories, type, long, disc):
         con = sqlite3.connect('workouts.db')
         cur = con.cursor()
-        query = f'UPDATE workout SET date = ?, calories = ?, WHERE id = ?'
-        cur.execute(query, (date, calories))
+        query = f'UPDATE workout SET date = ?, calories = ?, long = ?, type = ?, description = ? WHERE id = ?'
+        cur.execute(query, (date, calories, long, type, disc, work_id))
         con.commit()
         con.close()
 
@@ -233,7 +168,6 @@ class DataBase:
         cur = con.cursor()
         cur.execute('SELECT * FROM workout ORDER BY date DESC')
         return cur.fetchall()
-
 
 
 if __name__ == '__main__':
